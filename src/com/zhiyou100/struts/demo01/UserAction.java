b3567e0f223909b13e0b2556135b1096c51ae6a7
guorenjie <875654381@qq.com>
2017-09-08 23:18:35 +0800

    test

---
+++ src/com/zhiyou100/struts/demo01/UserAction.java
@@ -0,0 +1,13 @@
+package com.zhiyou100.struts.demo01;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport{
+
+	
+	@Override
+	public String execute() throws Exception {
+		
+		return "error";
+	}
+}
